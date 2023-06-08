0f06b1e182d5701cac563d4a8289d9686fceedd2
emilykamiti <emilykamiti@gmail.com>
2023-06-08 20:26:16 +0000

    mi

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -2,6 +2,6 @@ import { is } from 'immutable';
 
 // Create a function named areMapsEqual
 
-export default function areMapsEqual (map1, map2) {
+export default function areMapsEqual(map1, map2) {
   return is(map1, map2);
 }
