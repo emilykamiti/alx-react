d49add3116c8f572c19ae3f539f2e73e70283706
emilykamiti <emilykamiti@gmail.com>
2023-06-08 12:57:26 +0000

    mit

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export default function accessImmutableObject (object, array) {
+export default function accessImmutableObject(object, array) {
   const value = Map(object);
   return value.getIn(array);
 }
