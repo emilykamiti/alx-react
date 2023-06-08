54808ea9ca5d375417f06aaa0453cad22ba07fcb
emilykamiti <emilykamiti@gmail.com>
2023-06-08 20:07:54 +0000

    mit

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,5 +1,6 @@
-import { Map } from 'immutable':
+import { Map } from 'immutable';
 // Create a function named mergeDeeplyElements
 
 export default function mergeDeeplyElements(page1, page2) {
   return Map(page1).mergeDeep(Map(page2));
+}
