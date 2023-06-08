028a659c24424e41531083c01ed86774fde4f120
emilykamiti <emilykamiti@gmail.com>
2023-06-08 18:27:44 +0000

    mi

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,9 +1,9 @@
 import { List, Map } from 'immutable';
 
-export function concatElements (page1, page2) {
+export function concatElements(page1, page2) {
   return List(page1).concat(List(page2));
 }
 
-export function nmergeElements (page1, page2) {
+export function nmergeElements(page1, page2) {
   return Map(page1).merge(Map(page2));
 }
