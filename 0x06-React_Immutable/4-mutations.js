38cee83d15d0e053e811018e7fe5b58dbedd7610
emilykamiti <emilykamiti@gmail.com>
2023-06-08 17:34:33 +0000

    mit

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-export const map = new Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -12,4 +12,4 @@ export const map = new Map({
 export const map2 = map.map((value, key) => {
   if (key == 3) value = "Benjamin"
   if (key == 5 valu = "Oliver"
-});
+})
