3328884907d6def8adc626a72403be5ab796a082
emilykamiti <emilykamiti@gmail.com>
2023-06-08 11:41:23 +0000

    mit

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -1,16 +1,7 @@
-const { fromJS, Map } = require('immutable');
+const Immutable = require('immutable');
 
-function getImmutableObject(object) {
-  return fromJS(object);
+function getImmutableObject (object) {
+  return Immutable.fromJS(object);
 }
 
-// Example usage
-const object = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-const immutableMap = getImmutableObject(object);
-console.log(immutableMap);
+module.exports = getImmutableObject;
