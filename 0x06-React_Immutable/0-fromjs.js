c7bd335e40a6c30723a7f6b0fea8a644a447e29e
emilykamiti <emilykamiti@gmail.com>
2023-06-08 10:45:02 +0000

    mit

--- 0x06-React_Immutable/0-fromjs.js
+++ 0x06-React_Immutable/0-fromjs.js
@@ -5,14 +5,4 @@ function getImmutableObject(object) {
   return immutableMap;
 }
 
-const inputObject = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132,
-};
-
-const immutableMap = getImmutableObject(inputObject);
-console.log(immutableMap);
-
 module.exports = getImmutableObject;
