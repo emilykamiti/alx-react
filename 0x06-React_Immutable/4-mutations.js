f6105bc89a3e61473b3473ba077ed7c347e00c00
emilykamiti <emilykamiti@gmail.com>
2023-06-08 17:10:54 +0000

    mit

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -9,6 +9,6 @@ export const map = Map({
   6: 'Lucas',
 });
 
-export const map2 = map
-  .set(2, 'Benjamin')
-  .set(4, 'Oliver');
+export const map2 = map.withMutations((itemsToBeMapped) => {
+  itemsToBeMapped.set(2, 'Benjamin').set(4, 'Oliver);
+});
