1800b79c9aa3170f852001268768411672f9db6f
emilykamiti <emilykamiti@gmail.com>
2023-06-08 08:29:27 +0000

    mit

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,18 @@
+import { fromJS } from 'immutable';
+
+function getImmutableObject(object) {
+    const immutableMap = fromJS(object);
+    return immutableMap.toJS();
+}
+
+export default getImmutableObject;
+
+const object = {
+    fear: true,
+    smell: --1033575916.9145899,
+    wall: false,
+    thing: -914767132
+};
+
+const immutableMap = getImmutableObject(object);
+console.log(immutableMap);
