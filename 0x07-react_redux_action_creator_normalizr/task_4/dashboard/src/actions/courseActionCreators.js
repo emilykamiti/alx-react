0882b4b6e332955fcb8755059f554a8875b29a87
JoyNasinza <107001414+emilykamiti@users.noreply.github.com>
2023-06-15 20:50:41 +0300

    Add files via upload

---
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/actions/courseActionCreators.js
@@ -0,0 +1,27 @@
+import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+
+/**
+ * Action creator function to select a course.
+ * 
+ * @param {number} index - The index of the course to be selected.
+ * @returns {Object} An action object with the type and index.
+ */
+export const selectCourse = (index) => {
+  return {
+    type: SELECT_COURSE,
+    index,
+  };
+};
+
+/**
+ * Action creator function to unselect a course.
+ * 
+ * @param {number} index - The index of the course to be unselected.
+ * @returns {Object} An action object with the type and index.
+ */
+export const unSelectCourse = (index) => {
+  return {
+    type: UNSELECT_COURSE,
+    index,
+  };
+};
---
+++ 0x07-react_redux_action_creator_normalizr/task_4/dashboard/src/actions/uiActionCreators.js
@@ -0,0 +1,48 @@
+import { LOGIN, LOGOUT, DISPLAY_NOTIFICATION_DRAWER, HIDE_NOTIFICATION_DRAWER } from './uiActionTypes';
+
+/**
+ * Action creator function for the login action.
+ * 
+ * @param {string} email - The user's email.
+ * @param {string} password - The user's password.
+ * @returns {Object} An action object with the type and user information.
+ */
+export const login = (email, password) => {
+    return {
+        type: LOGIN,
+        user: { email, password }
+    }
+}
+
+/**
+ * Action creator function for the logout action.
+ * 
+ * @returns {Object} An action object with the type.
+ */
+export const logout = () => {
+    return {
+        type: LOGOUT,
+    }
+}
+
+/**
+ * Action creator function for displaying the notification drawer.
+ * 
+ * @returns {Object} An action object with the type.
+ */
+export const displayNotificationDrawer = () => {
+    return {
+        type: DISPLAY_NOTIFICATION_DRAWER,
+    }
+}
+
+/**
+ * Action creator function for hiding the notification drawer.
+ * 
+ * @returns {Object} An action object with the type.
+ */
+export const hideNotificataionDrawer = () => {
+    return {
+        type: HIDE_NOTIFICATION_DRAWER,
+    }
+}
