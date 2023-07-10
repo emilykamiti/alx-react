df35206966f22212129ca44033fb62f995587e2f
emilykamiti <emilykamiti@gmail.com>
2023-07-10 07:22:44 +0000

    Correction- counter is not constant

--- 0x00-Webpack/task_2/js/dashboard_main.js
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -8,11 +8,11 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append('<p id="count"></p>');
 $('body').append('<p>Copyright - Holberton School</p>');
 
-let conunt = 0;
+let counter = 0;
 
-function updateCounter() {
-  count++;
-  $("#count").html(`${count} clicks on the button`);
-};
+function updateCounter () {
+  counter++;
+  $('#count').html(`${counter} clicks on the button`);
+}
 
 $('button').on('click', _.debounce(updateCounter, 500));
